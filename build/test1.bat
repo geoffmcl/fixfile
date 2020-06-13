56936b9fab549a9901e542409ec320ad1fdcb68e
Geoff McLane <ubuntu@geoffair.info>
2020-06-13 16:25:33 +0200

    add a simple test case

---
+++ build/test1.bat
@@ -0,0 +1,23 @@
+@setlocal
+@set TMPEXE=Release\fixfile.exe
+@if NOT EXIST %TMPEXE% (
+@echo Can NOT locate %TMPEXE%! *** FIX ME ***
+@exit /b 1
+)
+
+@set TMPFIL1=test1.txt
+@set TMPFIL2=test1u.txt
+@set TMPFIL3=test1d.txt
+
+@echo Line 1 > %TMPFIL1%
+@echo Line 2 >> %TMPFIL1%
+@echo Line 3 >> %TMPFIL1%
+
+%TMPEXE% %TMPFIL1% -u -n -o-%TMPFIL2%
+
+%TMPEXE% %TMPFIL2% -n -o-%TMPFIL3%
+
+fc4 %TMPFIL1% %TMPFIL3%
+
+@rem eof
+
